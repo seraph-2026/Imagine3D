7231fcdcce4a68765de46f347aebfc595581f54c
Seraph-2026 <319324261+seraph-2026@users.noreply.github.com>
2026-08-22 18:11:08 +1200

    Removing build js files from the repo.

--- Code/Imagine3DView/build/js/I3D/index.js
+++
@@ -1,8 +0,0 @@
-export { Renderer } from "./Renderer/Renderer";
-export { CameraManager } from "./Renderer/CameraManager";
-export { Map } from "./Game/Map";
-export { Tile } from "./Game/Tile";
-export { AngleMapper } from "./Utilities/AngleMapper";
-export { CoordinateMapper } from "./Utilities/CoordinateMapper";
-export { I3DManager, manager } from "./I3DManager";
-//# sourceMappingURL=index.js.map
